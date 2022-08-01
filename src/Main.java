24962a6fb5e4eb2432ab127f56de7a8429f67d14
unknown <duongducchinh160@gmail.com>
2022-08-01 15:17:24 +0700

    lop Point2d vs lop Point3D

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(5,7) ;
+        System.out.println(point2D);
+        Point3D point3D = new Point3D(point2D.getX(), point2D.getY(), 5);
+        System.out.println(point3D);
+        System.out.println(Arrays.toString(point3D.getXYZ()));
+    }
+}
